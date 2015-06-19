d682e4c8f189ced54acbd443d691d7afbb66f47e
Nicholai de Guzman <nicholaideguzman@nisnic-imac.dhcp.swmed.org>
2015-06-19 10:55:37 -0500

    confirming to protocols

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchControllerDelegate, UISearchResultsUpdating {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    //can't add this via storyboard, need to be in code
+    var searchController:UISearchController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +24,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Mark - UI TableView data sourceimeplementating the delegates
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    //Mark - UISearchResultsUpdatingDelegate
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+    
+    }
+    
+    
+    
 
 }
 
