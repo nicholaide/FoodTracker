0ce8a013ad313aae23a86b7aae495aadd4eeca56
Nicholai de Guzman <nicholaideguzman@nisnic-imac.dhcp.swmed.org>
2015-06-19 10:42:34 -0500

    detail view controller setup

--- FoodTracker/DetailViewController.swift
+++ FoodTracker/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  FoodTracker
 //
 //  Created by Nicholai de Guzman on 6/19/15.
@@ -8,19 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func eatitBarButtonPressed(sender: UIBarButtonItem) {
+    }
 
 
 }
-
