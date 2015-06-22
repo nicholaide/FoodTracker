18a37af7ec962f2856dbefa4be3d9e2ae116c8db
Nicholai de Guzman <nicholaideguzman@nisnic-imac.dhcp.swmed.org>
2015-06-22 10:59:56 -0500

    fixed error with post request

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -128,32 +128,36 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
 //        task.resume() //executes our request
         
         
-        var request = NSMutableURLRequest(URL: NSURL(string: "https://api.nutrition.com/v1_1/search/")!)
+        //filters are dictionaries within dictionaries
+        var request = NSMutableURLRequest(URL: NSURL(string: "https://api.nutritionix.com/v1_1/search/")!)
         let session = NSURLSession.sharedSession()
         request.HTTPMethod = "POST"
         var params = [
             "appId" : kAppId,
             "appKey" : kAppKey,
             "fields" : ["item_name", "brand_name", "keywords", "usda_fields"],
-            "limit" : "50",
-            "query" : searchString,
-                //filters are dictionaries within dictionaries
-            "filters": ["exists":["usda_fields" : true ]]]
+            "limit"  : "50",
+            "query"  : searchString,
+            "filters": ["exists":["usda_fields": true]]]
         var error: NSError?
         request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &error)
-        
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         
         //completion handler will run after successful call, just like a thread
         var task = session.dataTaskWithRequest(request, completionHandler: { (data, response, err) -> Void in
-            
             var stringData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println(stringData)
             var conversionError: NSError?
-            var jsonDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableLeaves, error: &conversionError) as? NSDictionary // as? means this is an NSDictionary optional
+            var jsonDictionary = NSJSONSerialization.JSONObjectWithData(data, options:
+                NSJSONReadingOptions.MutableLeaves, error: &conversionError) as? NSDictionary
             println(jsonDictionary)
-        
+            // as? means this is an NSDictionary optional
         })
+        
+        
+        task.resume()
+        
         
     }
     
